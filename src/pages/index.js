c403c477021145ba76690d190b5cd7092dd6983f
eyucherin <yu24c@mtholyoke.edu>
2023-04-30 10:39:19 -0400

    added style to input box

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import UserInput from "./component/UserInput";
 export default function Home() {
   return (
     <div>
-      <div className="border w-[100%]">
+      <div className=" w-[100%]">
       <UserInput name="Process A" />
       </div>
       
