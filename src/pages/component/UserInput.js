01ef988b4c63e58aec31966bf305750d40090cca
eyucherin <yu24c@mtholyoke.edu>
2023-04-30 10:50:25 -0400

    fixed padding for input box

--- src/pages/component/UserInput.js
+++ src/pages/component/UserInput.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 const UserInput = (props) => {
   return (
-    <form className = " flex w-[100%] rounded-3xl bg-[#F8F3FE] shadow-[4px 4px 10px 5px rgba(0, 0, 0, 0.1)] drop-shadow-lg">
-        <div className = "text-2xl font-bold w-[33.33%]  mx-[3%] my-[1%] pl-[2%]">{props.name}</div>
-        <input id = "arrival_time" type = "text" className = "w-[33.33%] rounded-2xl mx-[3%] my-[1%] shadow-[2px 2px 2px 2px rgba(0, 0, 0, 0.1)] drop-shadow-md" ></input>
-        <input id = "burst_time" type = "text" className = "w-[33.33%]  rounded-2xl ml-[3%] my-[1%] mr-[5%] shadow-[2px 2px 2px 2px rgba(0, 0, 0, 0.1)] drop-shadow-md" ></input>
+    <form className = " flex w-[100%] rounded-full bg-[#F8F3FE] shadow-[4px 4px 10px 5px rgba(0, 0, 0, 0.1)] drop-shadow-lg">
+        <div className = "text-2xl font-bold w-[33.33%]  mx-[3%] my-[1%] pl-[2%] p-2">{props.name}</div>
+        <input id = "arrival_time" type = "text" className = "w-[33.33%] rounded-full mx-[3%] my-[1%] shadow-[2px 2px 2px 2px rgba(0, 0, 0, 0.1)] drop-shadow-md py-2 px-5" ></input>
+        <input id = "burst_time" type = "text" className = "w-[33.33%]  rounded-full ml-[3%] my-[1%] mr-[5%] shadow-[2px 2px 2px 2px rgba(0, 0, 0, 0.1)] drop-shadow-md py-2 px-5" ></input>
 
         {/* input with text */}
     </form>
