d438705a43d8a36cb80b8282cc5efa80daab8fb8
Joeun Park <park49j@mtholyoke.edu>
2023-05-06 11:54:59 -0400

    modification - mid stage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,25 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 import Image from "next/image";
-import { useState,useRef,useEffect } from "react";
+import { useState, useRef, useEffect } from "react";
 import UserInput from "./component/UserInput";
-import { useSelector } from 'react-redux';
+import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
-import { addProcess,getProcessState,getColor, setcpuBurst, setCurrentTime } from "./slices/processSlice";
+import {
+  addProcess,
+  getProcessState,
+  getColor,
+  setcpuBurst,
+  setCurrentTime,
+} from "./slices/processSlice";
 import Table from "./component/Table";
 import ProcessInfo from "./component/ProcessInfo";
 
-
-
 export default function Home() {
   const dispatch = useDispatch();
-  const processes = useSelector(state => state.processes);
+  const processes = useSelector((state) => state.processes);
   const scrollToRef = useRef(null);
-  const [num,setNum] = useState(1);
-  const [processList,setProcessList] = useState([]);
-
+  const [num, setNum] = useState(1);
+  const [processList, setProcessList] = useState([]);
 
   const colors = [
     "#F9F871",
@@ -28,7 +31,7 @@ export default function Home() {
     "#66BD56",
     "#9056BD",
     "#BD9056",
-  ]
+  ];
 
   const handleSimulateBtnClick = () => {
     if (scrollToRef.current) {
@@ -42,16 +45,18 @@ export default function Home() {
   };
 
   const handlePlusBtnClick = () => {
-    if (num < 10){
-      setNum(num+1);
-      setProcessList(processList.concat(<UserInput key={num} name={num+1} />));
+    if (num < 10) {
+      setNum(num + 1);
+      setProcessList(
+        processList.concat(<UserInput key={num} name={num + 1} />)
+      );
 
       const newProcess = {
-        id: num+1,
-        color: colors[num-1],
-        arrivalTime:0,
+        id: num + 1,
+        color: colors[num - 1],
+        arrivalTime: 0,
         cpuBurst: 0,
-        ioBurst:0,
+        ioBurst: 0,
         totalTime: 0,
         cpuVariance: 0,
         currentTime: 0,
@@ -61,9 +66,7 @@ export default function Home() {
     }
   };
 
-  const handleSimulateBtn = () => {
-  }
-
+  const handleSimulateBtn = () => {};
 
   return (
     <div className="flex flex-col bg-gradient-to-br from-[#E5CAFB] to-[#8A87C1]">
@@ -111,14 +114,23 @@ export default function Home() {
         </div>
         <div className="text-base w-96 pt-2">Provide your processes input</div>
         <div className="flex flex-col">
-          <div className="flex flex-col justify-center pt-14">
-            <div className = "flex flex-row mb-[1.5%] w-[100%] ">
-              <div class="basis-1/6 font-bold px-[4%]"></div>
-              <div class="basis-1/6 font-bold pl-[6%]">Arrival Time</div>
-              <div class="basis-1/6 font-bold pl-[7%]">IO Burst</div>
-              <div class="basis-1/6 font-bold pl-[5%]">CPU Burst</div>
-              <div class="basis-1/6 font-bold pl-[4%]">Total Time</div>
-              <div class="basis-1/6 font-bold pl-[3.5%]">Variance</div>
+          <div className="flex flex-col pt-14">
+            <div className="flex flex-row mb-[1.5%] w-[100%] ">
+              <div class="basis-1/6"></div>
+              <div class="basis-1/6  text-sm font-bold pl-[1.8%]">
+                Arrival Time
+              </div>
+              <div class="basis-1/6 text-sm font-bold pl-[2%]">CPU Burst</div>
+              <div class="basis-1/6 text-sm font-bold pl-[2.7%]">IO Burst</div>
+              <div class="basis-1/6 text-sm font-bold pl-[1.8%]">
+                Total Time
+              </div>
+              <div class="basis-1/6 text-sm font-bold pl-[1%]">
+                CPU Variance
+              </div>
+              <div class="basis-1/6 text-sm font-bold pl-[1.8%]">
+                IO Variance
+              </div>
             </div>
             <UserInput name={1} />
             {processList}
@@ -134,32 +146,33 @@ export default function Home() {
               height={58}
             />
           </button>
-        <button className = "flex justify-center" onClick={handleSimulateBtn} >
-          <a href = "#Visualization" className="mt-12">
-            <Image
-              src="/runBtn.png"
-              alt="Simulate button"
-              width={180}
-              height={84}
-            />
-          </a>
-        </button>
+          <button className="flex justify-center" onClick={handleSimulateBtn}>
+            <a href="#Visualization" className="mt-12">
+              <Image
+                src="/runBtn.png"
+                alt="Simulate button"
+                width={180}
+                height={84}
+              />
+            </a>
+          </button>
         </div>
       </div>
 
       <div className="pt-48 px-48">
-        <div className="text-5xl font-black" id = "Visualization">Visualization</div>
+        <div className="text-5xl font-black" id="Visualization">
+          Visualization
+        </div>
         <div className="text-base w-96 pt-2">This is how MLFQ works!</div>
-        <div className = "mt-5">Current Time</div>
+        <div className="mt-5">Current Time</div>
         <div className="my-[2%]">
-          <div className = "flex ">
-            <img src = {"priorityArrow.png"} className = "h-[41vh] "/>
-            <Table processNum = {num}/>
+          <div className="flex ">
+            <img src={"priorityArrow.png"} className="h-[41vh] " />
+            <Table processNum={num} />
           </div>
-          <ProcessInfo/>
+          <ProcessInfo />
         </div>
       </div>
-
     </div>
   );
 }
--- src/pages/slices/processSlice.js
+++ src/pages/slices/processSlice.js
@@ -1,94 +1,101 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = [
-    {
-      id: 1,
-      color: "#FF0000",
-      arrivalTime:0,
-      cpuBurst: 0,
-      ioBurst:0,
-      totalTime: 0,
-      cpuVariance: 0,
-      currentTime: 0,
-      priorityLevel: 0,
-    }
+  {
+    id: 1,
+    color: "#FF0000",
+    arrivalTime: 0,
+    cpuBurst: 0,
+    ioBurst: 0,
+    totalTime: 0,
+    cpuVariance: 0,
+    currentTime: 0,
+    priorityLevel: 0,
+  },
 ];
 
 const processSlice = createSlice({
-    name: 'processes',
-    initialState,
-    reducers: {
-      addProcess: (state, action) => {
-        state.push(action.payload);
-      },
-      setArrivalTime: (state, action) => {
-        const process = state.find(p => p.id === action.payload.id);
-        if (process) {
-          process.arrivalTime = action.payload.arrivalTime;
-        }
-      },
-      setcpuBurst: (state, action) => {
-          const process = state.find(p => p.id === action.payload.id);
-          if (process) {
-              process.cpuBurst = action.payload.cpuBurst;
-          }
-      },
-      setioBurst: (state, action) => {
-          const process = state.find(p => p.id === action.payload.id);
-          if (process) {
-              process.ioBurst = action.payload.ioBurst;
-          }
-      },
-      setStartTime: (state, action) => {
-          const process = state.find(p => p.id === action.payload.id);
-          if (process) {
-              process.startTime = action.payload.startTime;
-          }
-      },
-      setTotalTime: (state, action) => {
-          const process = state.find(p => p.id === action.payload.id);
-          if (process) {
-              process.totalTime = action.payload.totalTime;
-          }
-      },
-      setcpuVariance: (state, action) => {
-          const process = state.find(p => p.id === action.payload.id);
-          if (process) {
-              process.cpuVariance = action.payload.cpuVariance;
-          }
-      },
-      setCurrentTime:(state,action) => {
-            const process = state.find(p => p.id === action.payload.id);
-            if (process) {
-                process.currentTime = action.payload.currentTime;
-            }
-        },
-      getProcess: (state, action) => {
-          const process = state.find(p => p.id === action.payload.id);
-          if (process) {
-              return process;
-          }
-      },
-        getColor : (state, processId) => {
-        const process = state.find(p => p.id === action.payload.id);
-        return process.color;
+  name: "processes",
+  initialState,
+  reducers: {
+    addProcess: (state, action) => {
+      state.push(action.payload);
+    },
+    setArrivalTime: (state, action) => {
+      const process = state.find((p) => p.id === action.payload.id);
+      if (process) {
+        process.arrivalTime = action.payload.arrivalTime;
       }
     },
-  });
+    setCpuBurst: (state, action) => {
+      const process = state.find((p) => p.id === action.payload.id);
+      if (process) {
+        process.cpuBurst = action.payload.cpuBurst;
+      }
+    },
+    setIoBurst: (state, action) => {
+      const process = state.find((p) => p.id === action.payload.id);
+      if (process) {
+        process.ioBurst = action.payload.ioBurst;
+      }
+    },
+    setStartTime: (state, action) => {
+      const process = state.find((p) => p.id === action.payload.id);
+      if (process) {
+        process.startTime = action.payload.startTime;
+      }
+    },
+    setTotalTime: (state, action) => {
+      const process = state.find((p) => p.id === action.payload.id);
+      if (process) {
+        process.totalTime = action.payload.totalTime;
+      }
+    },
+    setCpuVariance: (state, action) => {
+      const process = state.find((p) => p.id === action.payload.id);
+      if (process) {
+        process.cpuVariance = action.payload.cpuVariance;
+      }
+    },
+    setIoVariance: (state, action) => {
+      const process = state.find((p) => p.id === action.payload.id);
+      if (process) {
+        process.ioVariance = action.payload.ioVariance;
+      }
+    },
+    setCurrentTime: (state, action) => {
+      const process = state.find((p) => p.id === action.payload.id);
+      if (process) {
+        process.currentTime = action.payload.currentTime;
+      }
+    },
+    getProcess: (state, action) => {
+      const process = state.find((p) => p.id === action.payload.id);
+      if (process) {
+        return process;
+      }
+    },
+    getColor: (state, processId) => {
+      const process = state.find((p) => p.id === action.payload.id);
+      return process.color;
+    },
+  },
+});
 
 export const getProcessState = (state) => state.processes;
 
 export const {
   addProcess,
   setArrivalTime,
-  setcpuBurst,
-  setioBurst,
+  setCpuBurst,
+  setIoBurst,
   setStartTime,
   setTotalTime,
-  setcpuVariance,
+  setCpuVariance,
+  setIoVariance,
   setCurrentTime,
   getProcess,
-  getColor
+  getColor,
 } = processSlice.actions;
 
 export default processSlice.reducer;
